559eac1f08593c672368bbb76b190883c4c668f8
EcodeJR <emmanueldcode@gmail.com>
2024-11-01 10:58:50 +0100

    cors testing

--- backend/app.js
+++ backend/app.js
@@ -11,13 +11,23 @@ const app = express();
 app.use(express.json());
 
 // Using CORS middleware to allow requests from specific origins
-const allowedOrigins = [`/^https:\/\/snap-solve-ecodejr\.vercel\.app$/`, "https://snap-solve-ecodejr.vercel.app", "*"];
-
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://snap-solve-ecodejr.vercel.app',
+];
 
 app.use(cors({
-  origin: allowedOrigins, // List of allowed origins
-  credentials: true
+  origin: (origin, callback) => {
+    // Allow requests with no origin, like mobile apps or Postman
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
 }));
+
 
 
 // Middleware to parse cookies
