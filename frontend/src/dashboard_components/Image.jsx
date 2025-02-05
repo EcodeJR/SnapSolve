bbb8bb9f1c5c6524002c2ea68bffac998029c63d
EcodeJR <emmanueldcode@gmail.com>
2025-02-05 16:15:44 +0100

    test

--- frontend/src/dashboard_components/Image.jsx
+++ frontend/src/dashboard_components/Image.jsx
@@ -30,7 +30,7 @@ const Image = ({ setHistoryUpdated }) => {
             const formData = new FormData();
             formData.append('image', imgArray[0]);
 
-            const res = await fetch('http://localhost:8080/main/image', {
+            const res = await fetch('http://localhost:8080/main/image', { //snap-solve-nine.vercel.app
                 method: 'POST',
                 headers,
                 body: formData,
